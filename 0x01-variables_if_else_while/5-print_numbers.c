44b75838610820005f723441bade3da2ef7defaf
MeritG <meritgeorge1@gmail.com>
2022-09-09 16:58:51 -0700

    C conding

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,14 +11,13 @@
 int main(void)
 {
 	int digit = 0;
-
-	 while (digit <= 9)
-	 {
-		 printf("%i", digit);
-		 ++digit;
-	 }
-	 printf("\n");
-
-	 return (0);
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+	
+	return (0);
 
 }
