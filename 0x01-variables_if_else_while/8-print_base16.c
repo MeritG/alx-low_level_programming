b7eaadd8bc8a2462f4a4f1d593426449ab2384af
MeritG <meritgeorge1@gmail.com>
2022-09-09 17:03:50 -0700

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 	int digit = 48;
-	/48; decimal rep of 0/
+	/*48; decimal rep of 0*/
 
-		while (digit <= 102) / 102; decimal rep of f/
+		while (digit <= 102) /* 102; decimal rep of f*/
 		{
 			putchar(digit);
 
