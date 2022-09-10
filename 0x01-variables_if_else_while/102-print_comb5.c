42ce965f8b86efeb99bf44e6822165b01d1f1c34
MeritG <meritgeorge1@gmail.com>
2022-09-09 18:45:00 -0700

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			putchar(a / 10 % 10 + '0');
 			putchar(a % 10 + '0');
 			putchar(' ');
-			putchar(b / 10 % 10 + '0'');
+			putchar(b / 10 % 10 + '0');
 			putchar(b % 10 + '0');
 			if (a == 98 && b == 99)
 			{
