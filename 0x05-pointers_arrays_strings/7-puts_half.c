5965fd115d694ac865247ea14757d6313c0aa9dd
MeritG <meritgeorge1@gmail.com>
2022-09-20 11:32:28 -0700

    more coding examples

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - print half a string
+ *
+ * @s: a pointer to a char
+ *
+ * Return: length of string
+ *
+ */
+ int _strlen(char *s)
+{
+	int a, b = 0;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+	a = b;
+	return (a);
+}
+
+/**
+ * puts_half - prints half a string
+ *
+ * @str: a pointer to a string
+ *
+ * Return: string
+ *
+ */
+
+void puts_half(char *str)
+{
+	int b;
+	int size = _strlen(str);
+
+	if (size % 2 != 0)
+	{
+		b = (size / 2) + 1;
+	}
+	else
+	{
+		b = size / 2;
+	}
+	for (b = b; b < size; b++)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
