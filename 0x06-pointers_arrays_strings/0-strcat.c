a7d71b9552774744725a4f1db915d83ad3e73177
MeritG <meritgeorge1@gmail.com>
2022-09-22 06:38:31 -0700

    more coding examples

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-		for (i = 0; dest[i] != '\0'; i++)
-		{
-			dest [i + j] = src[i];
-		}
-	dest[i + j] = '\0';
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
 	return (dest);
 }
