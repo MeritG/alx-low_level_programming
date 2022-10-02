fad62d32f6b37517fc409e6ba88914637daf4b8e
MeritG <meritgeorge1@gmail.com>
2022-10-02 06:43:14 -0700

    more coding examples

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the argu passed to it
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+	(void)argv;
+
+	for (c = 0; c <= argc; c++)
+	{}
+	printf("%\n", c - 2);
+
+	return (0);
+}
